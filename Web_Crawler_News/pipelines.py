709fb1fda3d39978c8d3d0da42e285d9382c8932
CarlosQuixada <carlos@trustvox.com.br>
2019-10-16 17:52:47 -0300

    Creating Pipeline

--- Web_Crawler_News/pipelines.py
+++ Web_Crawler_News/pipelines.py
@@ -5,7 +5,17 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
+
 
 class WebCrawlerNewsPipeline(object):
+    def open_spider(self, spider):
+        self.file = open('notices.txt', 'w')
+
+    def close_spider(self, spider):
+        self.file.close()
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + '\n'
+        self.file.write(line)
         return item
